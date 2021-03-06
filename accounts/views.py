d0c577e2a288cb4a0523c141ae35485427eb2e49
sangsan <sangsan.prohmvitak@gmail.com>
2021-03-07 00:13:55 +0700

    route to profile page
    + display user profile data

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'accounts/index.html', {})
+
+
+def profile(request):
+    return render(request, 'accounts/profile.html', {})
