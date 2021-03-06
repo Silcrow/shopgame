aebf9f1eb1054e3b7dfe1184f9a4796d211ff55e
sangsan <sangsan.prohmvitak@gmail.com>
2021-03-07 00:00:53 +0700

    Add login & logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('shop/', include('shop.urls')),
 ]
